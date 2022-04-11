8e74b01cfc3874d355712330b7d826201620614c
Rafael Negrello <rafael.negrello@gmail.com>
2022-04-10 23:49:52 -0300

    Aula 4

--- api/user/datasource/user.js
+++ api/user/datasource/user.js
@@ -22,6 +22,31 @@ class UsersAPI extends RESTDataSource {
     user.role = await this.get(`/roles/${user.role}`)
     return user
   }
+
+  async adicionaUser(user) {
+    const users = await this.get('/users')
+    user.id = users.length + 1
+    const role = await this.get(`roles?type=${user.role}`)
+    await this.post('users', {...user, role: role[0].id})
+    return ({
+      ...user,
+      role: role[0]
+    })
+  }
+
+  async atualizaUser(novosDados) {
+    const role = await this.get(`roles?type=${novosDados.role}`)
+    await this.put(`users/${novosDados.id}`, {...novosDados, role: role[0].id })
+    return ({
+      ...novosDados,
+      role: role[0]
+    })
+  }
+
+  async deletaUser(id) {
+    await this.delete(`users/${id}`)
+    return id
+  }
 }
 
 module.exports = UsersAPI
